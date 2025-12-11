d4946761a4400b480aaca9660cfac9345a76d225
HJ25 <70260230+Jung2010@users.noreply.github.com>
2025-12-11 16:06:39 +0900

    yoyoyoyoyo

--- popup.js
+++ popup.js
@@ -1,15 +1,62 @@
 // type : alert,confirm,prompt
 const popup = (function(){
     let count = 0;
-    return async function(type,title,text){
+    let closedCount = 0;
+    return async function(type,title="Title",text="Text",components={},bodyStyle={}){
+        const comps = {};
+
+        comps.main??={};
+        comps.main.mainColor??="lightgray";
+        comps.main.subColor??="gray";
+
+        comps.confirmBtn??={};
+        comps.confirmBtn.text??="OK";
+
         const $ = v=>document.querySelector(v);
         if(!(["alert","confirm","prompt"].includes(type)))
             throw Error("The pop-up type is invalid.\nOnly notification, confirmation, and prompt types are available.");
 
         document.body.innerHTML += `
-        <popup id="popup${++count}">
-        </popup>
+        <div id="popup${++count}">
+            <h1>${title}</h1>
+            <p>${text}</p>
+            <button id="confirmBtn${count}">
+                ${comps.confirmBtn.text}
+            </button>
+        </div>
         `;
+
+        const applyCss = (ele,style)=>{ // ele : dom, style : obj
+            Object.entries(style).forEach(v=>{
+                const [key,val] = v;
+                ele.style[key] = val;
+            });
+        };
+
+        const $pu = $("#popup"+count);
+        applyCss($pu,{
+            padding: "100px",
+            background: "none",
+            backdropFilter: "blur(50px)",
+            position: "fixed",
+            textAlign: "center",
+            bottom: "25%",
+            top: "25%",
+            left: "0px",
+            right: "0px"
+        });
+        applyCss($pu,bodyStyle);
+        applyCss($("#confirmBtn"+count),{
+            background: comps.main.mainColor,
+            border: "none",
+            padding: "10px",
+            fontSize: "20px",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            minWidth: "200px",
+            marginTop: "50px",
+            borderRadius: "10px"
+        });
         return false;
     }
 })();
--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const $ = v=>document.querySelector(v);
 $("#showPopup").onclick = ()=>alert();
 $("#showConfirm").onclick = ()=>confirm();
 $("#showPrompt").onclick = ()=>prompt();
+
+popup("alert");
